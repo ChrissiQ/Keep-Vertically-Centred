baa7c03946419bec46e1013255283c3268a4c350
Christina Klassen <chrissi@klssn.com>
2013-05-16 14:11:00 -0600

    First commit.

---
+++ keepverticallycentred.jquery.js
@@ -0,0 +1,38 @@
+/*
+
+Keep Vertically Centred jQuery Plugin
+
+© 2013 Chrissi Klassen
+
+Usage:
+	$('#yourElement').keepVerticallyCentred();
+
+Tips:
+	Elements must be positioned relatively.
+
+
+*/
+
+$.fn.keepVerticallyCentred = function(){
+
+	var element = this;
+	function vc(){
+		var parent = element.parent();
+		if (parent.is('body')){
+			parent = $(window);
+		}
+		var parentHeight = parent.height();
+		var elementHeight = element.height();
+		element.css({
+			"position": "relative", 
+			"top": ((parentHeight - elementHeight)/2) + "px"
+		});
+	}
+
+	vc(this,parent);
+
+	$(window).resize(function(){
+		vc(this,parent);
+	});
+
+};
